ed0ac15317fa6855478a572ded009b16da6269e5
THUMMA NAGENDRA KUMAR <thumma00001@gmail.com>
2025-03-29 20:07:31 +0530

    implement layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
+import HeaderBox from '@/components/Headerbox';
+import Headerbox from '@/components/Headerbox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {
+        firstName : 'Nagendra'
+    };
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header"> 
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName ||"Guest"}
+                subtext="Access and Manage your banking needs with Horizon efficiently."
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
